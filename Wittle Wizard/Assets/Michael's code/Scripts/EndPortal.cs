2121b8b6ae553e58bf95ac2017563661e8175e13
RidgeM <michaelridge80@gmail.com>
2022-04-25 19:22:42 -0400

    added a script to end game
    not yet attached to an object for the player to trigger

---
+++ Wittle Wizard/Assets/Michael's code/Scripts/EndPortal.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPortal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player")){
+            GameEnd.instance.TriggerEnd();
+        }     
+    }
+}
---
+++ Wittle Wizard/Assets/Michael's code/Scripts/GameEnd.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEnd : MonoBehaviour
+{
+    public static GameEnd instance;
+    public Image endImage;
+    Color imageColor;
+    float endTimer = 0f;
+    bool hasEnded = false;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        endImage.gameObject.SetActive(true);
+        imageColor = endImage.color;
+        imageColor.a = 0f;
+        endImage.color = imageColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasEnded){
+            endTimer += Time.deltaTime;
+            imageColor.a = endTimer/1;
+            if(endTimer > 2){
+                Application.Quit();
+            }
+        }
+    }
+
+    public void TriggerEnd()
+    {
+        hasEnded = true;
+    }
+}
