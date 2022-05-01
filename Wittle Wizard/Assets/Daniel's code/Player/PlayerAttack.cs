0b89e1f46a8c7da4a7da7eeefd723f6fc0c890f5
Daniel Udelhoven <dudelhoven@icloud.com>
2022-05-01 13:54:34 -0400

    All spells now cast in mouse direction

--- Wittle Wizard/Assets/Daniel's code/Player/PlayerAttack.cs
+++ Wittle Wizard/Assets/Daniel's code/Player/PlayerAttack.cs
@@ -7,7 +7,8 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] GameObject spellAttack1;
     [SerializeField] GameObject spellAttack2;
     public Spell activeSpell;
-    void Awake() {
+    void Awake()
+    {
         activeSpell = GetComponent<Player>().activeSpell;
     }
     void Update()
@@ -18,26 +19,30 @@ public class PlayerAttack : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))//check for input / left click
         {
-            if(GetComponent<Player>().activeSpell.cost <= HealthManaManager.instance.playerManaCurrent)
+            if (GetComponent<Player>().activeSpell.cost <= HealthManaManager.instance.playerManaCurrent)
             {
                 activeSpell = GetComponent<Player>().activeSpell;
+
+                Vector3 posInScreen = Camera.main.WorldToScreenPoint(transform.position);
+                Vector2 direction = (Input.mousePosition - posInScreen);
+                direction.Normalize();
                 if (transform.localScale.x > 0)//checks for direction we are facing
                 {
                     // activeSpell replaced spellAttack1
                     GameObject p = Instantiate(activeSpell.spellPrefab, transform.position - (transform.right), transform.rotation);
                     Rigidbody2D rb = p.GetComponent<Rigidbody2D>();
                     SpellStats stats = p.GetComponent<SpellStats>();
-                    rb.velocity = -transform.right * stats.speed;
+                    rb.velocity = direction * stats.speed;
                 }
                 else if (transform.localScale.x < 0)//checks for direction we are facing
                 {
                     GameObject p = Instantiate(activeSpell.spellPrefab, transform.position + (transform.right), transform.rotation);
                     Rigidbody2D rb = p.GetComponent<Rigidbody2D>();
                     SpellStats stats = p.GetComponent<SpellStats>();
-                    rb.velocity = transform.right * stats.speed;
+                    rb.velocity = direction * stats.speed;
                 }
             }
-            
+
         }
         /*if (Input.GetButtonDown("Fire2"))//check for input / right click
         {
