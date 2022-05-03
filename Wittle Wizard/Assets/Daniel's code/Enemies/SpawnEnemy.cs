4dbc825315502457b9844f00a7e0b9843e186c4e
Daniel Udelhoven <dudelhoven@icloud.com>
2022-05-03 12:22:41 -0400

    Fixed enemy spawns

--- Wittle Wizard/Assets/Daniel's code/Enemies/SpawnEnemy.cs
+++ Wittle Wizard/Assets/Daniel's code/Enemies/SpawnEnemy.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
+    float time = 0;
+    bool spawned = false;
     void Start()
     {
+        /*
         GameObject e = Instantiate(enemy, transform.position, Quaternion.identity);//spawn enemy
         EnemyTakeDamage stats = e.GetComponent<EnemyTakeDamage>();
-        stats.enemyColor = Random.Range(1, 3);//generate random color between 1 and 3 inclusive
+        stats.enemyColor = Random.Range(1, 4);//generate random color between 1 and 3 inclusive
+        */
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 0.6 && spawned == false)
+        {
+            GameObject e = Instantiate(enemy, transform.position, Quaternion.identity);//spawn enemy
+            EnemyTakeDamage stats = e.GetComponent<EnemyTakeDamage>();
+            stats.enemyColor = Random.Range(1, 4);//generate random color between 1 and 3 inclusive
+            spawned = true;
+        }
     }
 
 
